1229816efb97468423e0f5b85db72f1c62c7b6a4
SamuelT19 <samitesfay15@gmail.com>
2024-05-25 00:13:45 +0300

    backend endpoint added

--- app/endPoints/axios.jsx
+++ app/endPoints/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://tv-networks-server.onrender.com/",
 });
 export default axiosBase;
